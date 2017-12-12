2eabad35927c0ae66ed2df7bdab7204681d254b4
Thamizharasu <zarub2k@gmail.com>
2017-12-13 01:04:59 +0530

    App bar is added

--- lib/src/views/welcome.dart
+++ lib/src/views/welcome.dart
@@ -4,12 +4,28 @@ class TodoList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-      new Scaffold(
-        appBar: new AppBar(
-          leading: new IconButton(icon: new Icon(Icons.menu), onPressed: null),
-          title: new Text('Flutter Hustle'),
-        ),
-      );
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new IconButton(icon: new Icon(Icons.menu), onPressed: null),
+        title: new Text('Flutter Hustle'),
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null
+          )
+        ],
+      ),
+
+      body: new Center(
+        child: new Text('Welcome Flutter hustle!'),
+      ),
+
+      floatingActionButton: new FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Add',
+        child: new Icon(Icons.add),
+      ),
+    );
   }
 }

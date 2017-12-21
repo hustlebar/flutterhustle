50949a105a97139f220dcf7ade8249b3cf863401
Thamizharasu <zarub2k@gmail.com>
2017-12-22 00:04:18 +0530

    Chat message class introduced

---
+++ lib/src/friendlychat/chat_message.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+
+  Widget buildUi() {
+    return null;
+  }
+}

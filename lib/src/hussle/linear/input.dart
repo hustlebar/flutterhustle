9ee3f773c3bf4555939957667d8e9955acf448ed
Thamizharasu <zarub2k@gmail.com>
2018-01-07 23:43:43 +0530

    Input field added

--- lib/src/hussle/linear/input.dart
+++ lib/src/hussle/linear/input.dart
@@ -9,9 +9,41 @@ class Login extends StatefulWidget {
 }
 
 class LoginState extends State<Login> {
+  final TextEditingController _emailController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    return buildUi(context);
+  }
+
+  Widget buildUi(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+
+        new Center(
+          child: new TextField(
+            controller: _emailController,
+          ),
+        ),
+
+        new Center(
+          child: new RaisedButton(
+            child: new Text('Check'),
+            onPressed: _onClicked,
+          ),
+        )
+
+
+
+      ],
+    );
+  }
+
+  void _onClicked() {
+    print('Button clicked');
+    print(_emailController.text);
+    setState(() {
+      
+    });
   }
 }

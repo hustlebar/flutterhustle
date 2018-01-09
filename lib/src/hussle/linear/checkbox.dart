7c35234985ea5068f7b1e47b2c673d0c1f2b3539
Thamizharasu <zarub2k@gmail.com>
2018-01-09 23:26:34 +0530

    Checkbox work started

--- lib/src/hussle/linear/checkbox.dart
+++ lib/src/hussle/linear/checkbox.dart
@@ -1,9 +1,62 @@
 part of hussle;
 
-class Checkbox extends StatelessWidget {
+class HussleCheckboxWithTitle extends StatelessWidget {
+  bool rogerVal = false;
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    return new Column(
+      children: <Widget>[
+        new Center(
+          child: new CheckboxListTile(
+            title: new Text('Roger'),
+            value: rogerVal,
+            onChanged: (bool value) {
+              
+            }
+          )
+        )
+      ],
+    );
+  }
+
+  void _onRoger(bool value) {
+    rogerVal = value;
+    print('On Roger: $rogerVal');
+  }
+}
+
+class HussleCheckbox extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return new HussleCheckboxState();
+  }
+}
+
+class HussleCheckboxState extends State<HussleCheckbox> {
+  bool checkboxValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return buildUi();
+  }
+
+  Widget buildUi() {
+    return new Column(
+      children: <Widget>[
+        new Center(
+          child: new Checkbox(
+            value: checkboxValue,
+            onChanged: (bool value) {
+              setState(() {
+                checkboxValue = value;
+                print('Checkbox value: $checkboxValue');
+              });
+            }
+          ),
+        )
+      ],
+    );
   }
 }

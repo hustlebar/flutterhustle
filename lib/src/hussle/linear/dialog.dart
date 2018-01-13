6540eb016ca128d579abdd191fc45f3e558ae09d
Thamizharasu S <zarub2k@gmail.com>
2018-01-13 16:00:06 +0530

    dialog tried

--- lib/src/hussle/linear/dialog.dart
+++ lib/src/hussle/linear/dialog.dart
@@ -1,1 +1,66 @@
 part of hussle;
+
+class HussleDialog extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return buildUi(context);
+  }
+
+  Widget buildUi(BuildContext context) {
+    var button = new RaisedButton(
+      child: new Text('Show dialog'),
+      onPressed: () => _onClick(context)
+    );
+
+    return new Column(
+      children: <Widget>[
+        new Center(
+          child: button,
+        )
+      ],
+    );
+  }
+
+  void _onClick(BuildContext context) {
+    print('Enters _onClick()');
+    onDialog(context);
+  }
+
+  void onDialog(BuildContext context) {
+    var dialog = new SimpleDialog(
+      title: const Text('Select your location'),
+      children: <Widget>[
+        new SimpleDialogOption(
+          child: new Text(Location.Andhra.toString()),
+        ),
+
+        new SimpleDialogOption(
+          child: new Text(Location.Chennai.toString()),
+        ),
+
+        new SimpleDialogOption(
+          child: new Text(Location.Pondycherry.toString()),
+        ),
+
+        new SimpleDialogOption(
+          child: new Text(Location.Kerala.toString()),
+        ),
+
+        new SimpleDialogOption(
+          child: new Text(Location.Karnataka.toString()),
+        )
+
+      ],
+    );
+
+    showDialog<Location>(
+      context: context,
+      child: dialog
+    ).then((Location selected) {
+      print('Selected location: $selected');
+    });
+  }
+}
+
+enum Location {Chennai, Pondycherry, Kerala, Andhra, Karnataka}

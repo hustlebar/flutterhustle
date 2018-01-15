f27cd045e89fe6c71adcce0a6a291853ac787742
Thamizharasu <zarub2k@gmail.com>
2018-01-16 00:02:57 +0530

    Code commented

--- lib/src/hussle/layout/hussle_scaffold.dart
+++ lib/src/hussle/layout/hussle_scaffold.dart
@@ -4,12 +4,33 @@ class HussleScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+//    Navigator.of(context).push(
+//      new MaterialPageRoute<Null>(
+//        builder: (BuildContext context) {
+//          return new Scaffold(
+//            appBar: new AppBar(title: new Text('Hussle Bar'),),
+//            body: new MainPage(),
+//            floatingActionButton: new FloatingActionButton(
+//              child: new Icon(Icons.add),
+//              onPressed: () {
+//                Navigator.of(context).pop();
+//              }
+//            ),
+//          );
+//        }
+//      )
+//    );
+//
+//    return null;
+
     return new Scaffold(
       appBar: new AppBar(title: new Text('Hussle Bar'),),
       body: new MainPage(),
       floatingActionButton: new FloatingActionButton(
         child: new Icon(Icons.add),
-        onPressed: onActionClicked
+        onPressed: () {
+          Navigator.of(context).pop();
+        }
       ),
     );
   }

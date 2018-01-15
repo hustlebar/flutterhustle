0eb2a272ac964205fe0116309a6fca0880e6099f
Thamizharasu <zarub2k@gmail.com>
2018-01-15 06:45:52 +0530

    Button bar added into the cards

--- lib/src/hussle/linear/card.dart
+++ lib/src/hussle/linear/card.dart
@@ -21,9 +21,26 @@ class HussleCard extends StatelessWidget {
     return new Card(
       child: new Column(
         children: <Widget>[
+
           new ListTile(
             leading: const Icon(Icons.account_box),
-            title: const Text('Card'),
+            title: const Text('Do you want to buy?'),
+          ),
+
+          new ButtonTheme.bar(
+            child: new ButtonBar(
+              children: <Widget>[
+                new FlatButton(
+                  onPressed: () => print('Button buy clicked'),
+                  child: const Text('Buy')
+                ),
+
+                new FlatButton(
+                  onPressed: () => print('Button cancel clicked'),
+                  child: const Text('Cancel')
+                )
+              ],
+            ),
           )
 
 
